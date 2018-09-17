ffad6bfa1a6e0a889bed100f76c9c3da7c507127
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-09-17 18:41:53 -0400

    Changed IT, to reload server between each test

--- src/it/java/ca/ulaval/glo4003/ws/api/PingResourceIT.java
+++ src/it/java/ca/ulaval/glo4003/ws/api/PingResourceIT.java
@@ -1,11 +1,16 @@
 package ca.ulaval.glo4003.ws.api;
 
-import static org.hamcrest.Matchers.any;
+import ca.ulaval.glo4003.StartServerRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.any;
 
 public class PingResourceIT {
+
+    @Rule
+    public StartServerRule startServer = new StartServerRule();
 
     @Test
     public void givenApplicationBooted_whenPinging_thenApplicationRespondWithVersion() {
