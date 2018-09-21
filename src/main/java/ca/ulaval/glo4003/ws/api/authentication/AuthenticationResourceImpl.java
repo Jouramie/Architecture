1248f94f203283fc5fa5dcacc1f629bff33f5357
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-09-21 12:10:24 -0400

    Swagger authentication route

--- src/main/java/ca/ulaval/glo4003/ws/api/authentication/AuthenticationResourceImpl.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/authentication/AuthenticationResourceImpl.java
@@ -1,6 +1,7 @@
 package ca.ulaval.glo4003.ws.api.authentication;
 
-public class AuthenticationResourceImpl implements AuthenticationResource{
+public class AuthenticationResourceImpl implements AuthenticationResource {
+
     @Override
     public AuthenticationResponseDto authenticate(AuthenticationRequestDto authenticationRequest) {
         return null;
