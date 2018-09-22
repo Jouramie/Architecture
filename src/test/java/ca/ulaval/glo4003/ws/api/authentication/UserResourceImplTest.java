a5f8ad5bf3037e0fe2797aa57004a078a1052681
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-09-22 16:38:37 -0400

    Resource test

---
+++ src/test/java/ca/ulaval/glo4003/ws/api/authentication/UserResourceImplTest.java
@@ -0,0 +1,31 @@
+package ca.ulaval.glo4003.ws.api.authentication;
+
+import ca.ulaval.glo4003.ws.application.user.UserCreationService;
+import ca.ulaval.glo4003.ws.domain.user.UserRole;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserResourceImplTest {
+
+    public static final UserCreationDto CREATION_REQUEST =
+            new UserCreationDto("username", "password", UserRole.ADMINISTRATOR);
+
+    @Mock
+    private UserCreationService userCreationService;
+
+    @InjectMocks
+    private UserResourceImpl userResource;
+
+    @Test
+    public void whenCreatingUser_thenUserIsCreated() {
+        userResource.createUser(CREATION_REQUEST);
+
+        verify(userCreationService).createUser(CREATION_REQUEST);
+    }
+}
