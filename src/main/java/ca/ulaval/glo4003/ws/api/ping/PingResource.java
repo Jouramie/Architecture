73647296a926da589e868dc81635cb5432678213
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-09-30 00:13:19 -0400

    removed wildcards

--- src/main/java/ca/ulaval/glo4003/ws/api/ping/PingResource.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/ping/PingResource.java
@@ -5,7 +5,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/ping")
