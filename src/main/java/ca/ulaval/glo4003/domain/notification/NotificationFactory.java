418f868395cf164c838bbd9e7a933623ad87be53
OLBEA20 <olivier.beaulieu.7@ulaval.ca>
2018-10-05 10:44:34 -0400

    NotificationFactory messageEntry formatting

--- src/main/java/ca/ulaval/glo4003/domain/notification/NotificationFactory.java
+++ src/main/java/ca/ulaval/glo4003/domain/notification/NotificationFactory.java
@@ -8,6 +8,9 @@ import ca.ulaval.glo4003.domain.transaction.TransactionItem;
 import java.util.List;
 
 public class NotificationFactory {
+
+  private static final String TRANSACTION_ENTRY_FORMAT = "%s %sx %s %s : %s%s";
+
   public Notification create(Transaction transaction) {
     return new Notification(buildNotificationTitle(transaction),
         buildTransactionSummary(transaction));
@@ -19,17 +22,17 @@ public class NotificationFactory {
 
   private String buildTransactionSummary(Transaction transaction) {
     List<String> entries = transaction.getListItems().stream()
-        .map(this::buildMessageEntry)
+        .map(this::buildTransactionEntry)
         .collect(toList());
     entries.add(buildTotalEntry(transaction));
     return String.join("\n", entries);
   }
 
-  private String buildMessageEntry(TransactionItem item) {
+  private String buildTransactionEntry(TransactionItem item) {
     MoneyAmount total = item.amount.multiply(item.quantity);
-    return item.stockId + " " + item.quantity + "x " + item.amount.getAmount()
-        + item.amount.getCurrency().getName() + " " + ": "
-        + total.getAmount() + total.getCurrency().getName();
+    return String.format(TRANSACTION_ENTRY_FORMAT, item.stockId, item.quantity,
+        item.amount.getAmount(), item.amount.getCurrency().getName(), total.getAmount(),
+        total.getCurrency().getName());
   }
 
   private String buildTotalEntry(Transaction transaction) {
