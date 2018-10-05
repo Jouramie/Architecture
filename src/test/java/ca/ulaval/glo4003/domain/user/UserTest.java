67be6f89d484bb942097323091fd4e9d87b8255c
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-10-05 16:01:05 -0400

    fix some_(body)4email

--- src/test/java/ca/ulaval/glo4003/domain/user/UserTest.java
+++ src/test/java/ca/ulaval/glo4003/domain/user/UserTest.java
@@ -2,22 +2,21 @@ package ca.ulaval.glo4003.domain.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import ca.ulaval.glo4003.domain.user.authentication.AuthenticationTokenFactory;
 import ca.ulaval.glo4003.util.UserBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
 public class UserTest {
 
-  private static final String SOME_4EMAIL = "a email";
+  private static final String SOME_EMAIL = "4email@email.com";
   private static final String SOME_PASSWORD = "a password";
   private static final String WRONG_PASSWORD = SOME_PASSWORD + "wrong";
-  private final AuthenticationTokenFactory tokenFactory = new AuthenticationTokenFactory();
+
   private User user;
 
   @Before
   public void initialize() {
-    user = new UserBuilder().withEmail(SOME_4EMAIL).withPassword(SOME_PASSWORD).build();
+    user = new UserBuilder().withEmail(SOME_EMAIL).withPassword(SOME_PASSWORD).build();
   }
 
   @Test
