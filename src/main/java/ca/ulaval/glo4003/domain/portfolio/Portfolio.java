f75c09f5e43b8f8268d1062f18da082975bbaea4
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2018-10-18 01:05:14 -0400

    Implemented the class Portfolio.

--- src/main/java/ca/ulaval/glo4003/domain/portfolio/Portfolio.java
+++ src/main/java/ca/ulaval/glo4003/domain/portfolio/Portfolio.java
@@ -1,12 +1,76 @@
 package ca.ulaval.glo4003.domain.portfolio;
 
+import static java.util.stream.Collectors.toList;
+
+import ca.ulaval.glo4003.domain.money.Currency;
+import ca.ulaval.glo4003.domain.money.MoneyAmount;
+import ca.ulaval.glo4003.domain.stock.Stock;
+import ca.ulaval.glo4003.domain.stock.StockCollection;
+import ca.ulaval.glo4003.domain.stock.StockNotFoundException;
+import ca.ulaval.glo4003.domain.stock.StockRepository;
+import java.math.BigDecimal;
+import java.util.List;
+
 public class Portfolio {
+  private final StockRepository stockRepository;
+  private StockCollection stocks;
 
+  public Portfolio(StockRepository stockRepository) {
+    stocks = new StockCollection();
+    this.stockRepository = stockRepository;
+  }
 
-  public void add(String title, int quantity) {
+  public void add(String title, int quantity) throws StockNotFoundException {
+    checkIfStockExists(title);
+    stocks = stocks.add(title, quantity);
   }
 
   public int getQuantity(String title) {
-    return 0;
+    return stocks.getQuantity(title);
+  }
+
+  public MoneyAmount getCurrentTotalValue() {
+    List<Stock> stockList = stocks.getStocks().stream().map(this::tryGetStockByTitle)
+        .collect(toList());
+
+    Currency currency = getFirstStockCurrencyOrDefault(stockList);
+    MoneyAmount currentTotalValue = MoneyAmount.zero(currency);
+    for (Stock stock : stockList) {
+      currentTotalValue = currentTotalValue.add(getSubtotal(stock));
+    }
+
+    return currentTotalValue;
+  }
+
+  private void checkIfStockExists(String title) throws StockNotFoundException {
+    stockRepository.getByTitle(title);
+  }
+
+  private MoneyAmount getSubtotal(Stock stock) {
+    MoneyAmount currentValue = stock.getValue().getCurrentValue();
+    int quantity = stocks.getQuantity(stock.getTitle());
+    return currentValue.multiply(quantity);
+  }
+
+  private Stock tryGetStockByTitle(String title) {
+    Stock stock;
+
+    try {
+      stock = stockRepository.getByTitle(title);
+    } catch (StockNotFoundException e) {
+      throw new RuntimeException();
+    }
+
+    return stock;
+  }
+
+  private Currency getFirstStockCurrencyOrDefault(List<Stock> stockList) {
+    Currency currency = new Currency("USD", new BigDecimal(1));
+
+    if (!stockList.isEmpty()) {
+      currency = stockList.get(0).getValue().getCurrentValue().getCurrency();
+    }
+
+    return currency;
   }
 }
--- src/test/java/ca/ulaval/glo4003/domain/portfolio/PortfolioTest.java
+++ src/test/java/ca/ulaval/glo4003/domain/portfolio/PortfolioTest.java
@@ -1,25 +1,83 @@
 package ca.ulaval.glo4003.domain.portfolio;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.BDDMockito.willReturn;
+import static org.mockito.BDDMockito.willThrow;
 
+import ca.ulaval.glo4003.domain.money.Currency;
+import ca.ulaval.glo4003.domain.money.MoneyAmount;
+import ca.ulaval.glo4003.domain.stock.Stock;
+import ca.ulaval.glo4003.domain.stock.StockNotFoundException;
+import ca.ulaval.glo4003.domain.stock.StockRepository;
+import ca.ulaval.glo4003.domain.stock.StockValue;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class PortfolioTest {
   private final String SOME_TITLE = "MSFT";
+  private final String SOME_INVALID_TITLE = "invalid";
   private final int SOME_QUANTITY = 3;
+  private final String SOME_CURRENCY_NAME = "CAD";
+  private final BigDecimal SOME_RATE_TO_USD = new BigDecimal(1);
+  private final Currency SOME_CURRENCY = new Currency(SOME_CURRENCY_NAME, SOME_RATE_TO_USD);
+  private final MoneyAmount SOME_VALUE = new MoneyAmount(12.2, SOME_CURRENCY);
+  private final StockValue SOME_STOCK_VALUE = new StockValue(SOME_VALUE, SOME_VALUE, SOME_VALUE);
+
+  @Mock
+  private Stock someStock;
+
+  @Mock
+  private StockRepository someStockRepository;
 
   private Portfolio portfolio;
 
   @Before
-  public void setupPortfolio() {
-    portfolio = new Portfolio();
+  public void setupPortfolio() throws StockNotFoundException {
+    portfolio = new Portfolio(someStockRepository);
+
+    willReturn(SOME_STOCK_VALUE).given(someStock).getValue();
+    willReturn(SOME_TITLE).given(someStock).getTitle();
+
+    willReturn(someStock).given(someStockRepository).getByTitle(SOME_TITLE);
+    willThrow(StockNotFoundException.class).given(someStockRepository).getByTitle(SOME_INVALID_TITLE);
   }
 
   @Test
-  public void whenAddStockToPortfolio_thenItCanBeRetrieved() {
+  public void whenAddStockToPortfolio_thenItCanBeRetrieved() throws StockNotFoundException {
     portfolio.add(SOME_TITLE, SOME_QUANTITY);
 
     assertThat(portfolio.getQuantity(SOME_TITLE)).isEqualTo(SOME_QUANTITY);
+  }
+
+  @Test
+  public void givenStockDoesNotExist_whenAddStockToPortfolio_thenAnExceptionIsThrown() {
+    assertThatExceptionOfType(StockNotFoundException.class)
+        .isThrownBy(() -> portfolio.add(SOME_INVALID_TITLE, SOME_QUANTITY));
+  }
+
+  @Test
+  public void givenStockNotInPortfolio_whenGetQuantity_thenReturnZero() {
+    assertThat(portfolio.getQuantity(SOME_TITLE)).isEqualTo(0);
+  }
+
+  @Test
+  public void givenPortfolioNotEmpty_whenGetCurrentTotalValue_thenReturnSumOfItemValues() throws StockNotFoundException {
+    portfolio.add(SOME_TITLE, SOME_QUANTITY);
+
+    BigDecimal currentTotal = SOME_VALUE.getAmount().multiply(new BigDecimal(SOME_QUANTITY));
+    assertThat(portfolio.getCurrentTotalValue().getAmount()).isEqualTo(currentTotal);
+  }
+
+  @Test
+  public void givenPortfolioIsEmpty_whenGetCurrentTotalValue_thenReturnZero() {
+    BigDecimal currentTotal = new BigDecimal(0).setScale(2, RoundingMode.HALF_EVEN);
+    assertThat(portfolio.getCurrentTotalValue().getAmount()).isEqualTo(currentTotal);
   }
 }
