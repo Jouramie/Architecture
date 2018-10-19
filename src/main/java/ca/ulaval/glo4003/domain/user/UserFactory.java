edd2412b5c4cce97e6c2c8cf3c9b6b6efaf1f24b
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2018-10-18 21:23:19 -0400

    Added missing @Inject

--- src/main/java/ca/ulaval/glo4003/domain/user/UserFactory.java
+++ src/main/java/ca/ulaval/glo4003/domain/user/UserFactory.java
@@ -2,12 +2,14 @@ package ca.ulaval.glo4003.domain.user;
 
 import ca.ulaval.glo4003.domain.stock.StockRepository;
 import ca.ulaval.glo4003.infrastructure.injection.Component;
+import javax.inject.Inject;
 
 @Component
 public class UserFactory {
 
   private final StockRepository stockRepository;
 
+  @Inject
   public UserFactory(StockRepository stockRepository) {
     this.stockRepository = stockRepository;
   }
