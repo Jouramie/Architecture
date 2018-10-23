59af69557d10eb13764ce28d9d80cf3b21b8b40d
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-10-23 09:01:01 -0400

    reformat portfolio doc

--- src/main/java/ca/ulaval/glo4003/ws/api/portfolio/PortfolioItemResponseDto.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/portfolio/PortfolioItemResponseDto.java
@@ -4,15 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 
 @Schema(
-    name = "PortfolioItemResponseDto",
-    description = "Portfolio stock response containing the title, current value and quantity of a stock."
+    name = "Portfolio item"
 )
 public class PortfolioItemResponseDto {
-  @Schema(description = "Title")
   public final String title;
-  @Schema(description = "Current value")
   public final BigDecimal currentValue;
-  @Schema(description = "Quantity")
   public final int quantity;
 
   public PortfolioItemResponseDto(String title, BigDecimal currentValue, int quantity) {
