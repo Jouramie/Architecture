cadef78ca2b915b8b88372ce45bba64de42897a0
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-10-23 11:00:57 -0400

    Move GetStockMax to it's own file

--- src/main/java/ca/ulaval/glo4003/ws/api/stock/StockResource.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/stock/StockResource.java
@@ -1,7 +1,5 @@
 package ca.ulaval.glo4003.ws.api.stock;
 
-import ca.ulaval.glo4003.service.stock.max.StockMaxValueSinceRange;
-import ca.ulaval.glo4003.ws.api.stock.max.StockMaxResponseDto;
 import ca.ulaval.glo4003.ws.http.pagination.PaginationBinding;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -91,40 +89,4 @@ public interface StockResource {
           )
       )
           int perPage);
-
-  @GET
-  @Path("/{title}/max")
-  @Operation(summary = "Get the maximum value for a given stock title.",
-      description = "Return the stock maximum value since the interval asked.",
-      responses = {
-          @ApiResponse(
-              responseCode = "200",
-              content = @Content(
-                  schema = @Schema(
-                      implementation = StockMaxResponseDto.class
-                  )
-              )
-          ),
-          @ApiResponse(
-              responseCode = "400",
-              description = "'since' parameter is missing or invalid."
-          ),
-          @ApiResponse(
-              responseCode = "404",
-              description = "Stock does not exist."
-          )
-      }
-  )
-  StockMaxResponseDto getStockMaxValue(
-      @PathParam("title")
-          String title,
-      @QueryParam("since")
-      @Parameter(
-          description = "Specify a range where the maximum value will be searched for.",
-          schema = @Schema(
-              implementation = StockMaxValueSinceRange.class
-          ),
-          required = true
-      )
-          String since);
 }
