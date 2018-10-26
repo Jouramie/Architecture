241b4b8b8e7e6b3f583af08eb45909b2f34b1dd4
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2018-10-26 00:01:54 -0400

    Added constructor with default currency in MoneyAmount.

--- src/main/java/ca/ulaval/glo4003/domain/money/MoneyAmount.java
+++ src/main/java/ca/ulaval/glo4003/domain/money/MoneyAmount.java
@@ -8,6 +8,10 @@ public class MoneyAmount {
   private final BigDecimal amount;
   private final Currency currency;
 
+  public MoneyAmount(double amount) {
+    this(amount, Currency.USD);
+  }
+
   public MoneyAmount(double amount, Currency currency) {
     this.amount = new BigDecimal(amount).setScale(2, RoundingMode.HALF_EVEN);
     this.currency = currency;
