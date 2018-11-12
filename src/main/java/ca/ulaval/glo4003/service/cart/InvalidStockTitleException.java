f9d884bb9300c5ef9236de16d7eb9e5f415611fa
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-11-12 12:58:34 -0500

    exist

--- src/main/java/ca/ulaval/glo4003/service/cart/InvalidStockTitleException.java
+++ src/main/java/ca/ulaval/glo4003/service/cart/InvalidStockTitleException.java
@@ -2,7 +2,7 @@ package ca.ulaval.glo4003.service.cart;
 
 public class InvalidStockTitleException extends RuntimeException {
   public InvalidStockTitleException(String title) {
-    super("Stock " + title + " does not exists.");
+    super("Stock " + title + " does not exist.");
   }
 
   public InvalidStockTitleException(Exception cause) {
