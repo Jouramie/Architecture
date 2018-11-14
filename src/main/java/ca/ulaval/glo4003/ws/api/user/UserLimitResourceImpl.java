43c8283a07b017cf21b945652e5a73443590e696
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-11-14 13:48:42 -0500

    remove annotations in impl

--- src/main/java/ca/ulaval/glo4003/ws/api/user/UserLimitResourceImpl.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/user/UserLimitResourceImpl.java
@@ -4,7 +4,6 @@ import ca.ulaval.glo4003.ws.api.user.dto.ApiUserLimitDto;
 import ca.ulaval.glo4003.ws.api.user.dto.UserMoneyAmountLimitCreationDto;
 import ca.ulaval.glo4003.ws.api.user.dto.UserStockLimitCreationDto;
 import javax.annotation.Resource;
-import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 
 @Resource
@@ -12,13 +11,14 @@ public class UserLimitResourceImpl implements UserLimitResource {
 
   @Override
   public ApiUserLimitDto setUserStockLimit(String email,
-                                           @Valid UserStockLimitCreationDto userStockLimitCreationDto) {
+                                           UserStockLimitCreationDto userStockLimitCreationDto) {
+
     return null;
   }
 
   @Override
   public ApiUserLimitDto setUserMoneyAmountLimit(String email,
-                                                 @Valid UserMoneyAmountLimitCreationDto userMoneyAmountLimitCreationDto) {
+                                                 UserMoneyAmountLimitCreationDto userMoneyAmountLimitCreationDto) {
     return null;
   }
 
