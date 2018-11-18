4858d42ecc8eccbd376e43b0a20dc41f7ba592f5
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-11-18 18:19:54 -0500

    reactivate authenticationRequiredBinding

--- src/main/java/ca/ulaval/glo4003/ws/api/user/UserResource.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/user/UserResource.java
@@ -32,7 +32,7 @@ import javax.ws.rs.core.Response;
 public interface UserResource {
 
   @GET
-  // @AuthenticationRequiredBinding
+  @AuthenticationRequiredBinding
   @Operation(
       summary = "Get all users.",
       description = "Return all users, with their information.",
@@ -63,7 +63,7 @@ public interface UserResource {
 
   @GET
   @Path("/{email}")
-  // @AuthenticationRequiredBinding
+  @AuthenticationRequiredBinding
   @Operation(
       summary = "Get a user for a given email.",
       description = "Return the details of the user with the corresponding email.",
