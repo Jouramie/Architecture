cb3e09e0b731037705bc4ab08e5bd722eee09c43
simonpierredeschenes <simon-pierre.deschenes.1@ulaval.ca>
2018-11-20 22:33:21 -0500

    Implemented PortfolioReportIT and fixed PortfolioReportITContext.

--- src/it/java/ca/ulaval/glo4003/portfolio/PortfolioReportITContext.java
+++ src/it/java/ca/ulaval/glo4003/portfolio/PortfolioReportITContext.java
@@ -2,6 +2,8 @@ package ca.ulaval.glo4003.portfolio;
 
 import ca.ulaval.glo4003.context.AbstractContext;
 import ca.ulaval.glo4003.domain.clock.Clock;
+import ca.ulaval.glo4003.domain.notification.NotificationSender;
+import ca.ulaval.glo4003.domain.notification.NullNotificationSender;
 import ca.ulaval.glo4003.domain.user.CurrentUserSession;
 import ca.ulaval.glo4003.domain.user.User;
 import ca.ulaval.glo4003.domain.user.UserNotFoundException;
@@ -15,15 +17,13 @@ public class PortfolioReportITContext extends AbstractContext {
   @Override
   public void configureApplication(String apiUrl) {
     super.configureApplication(apiUrl);
+    serviceLocator.register(NotificationSender.class, NullNotificationSender.class);
 
-    LocalDate currentDate = serviceLocator.get(Clock.class).getCurrentTime().toLocalDate();
-    addStock("MSFT", 24, currentDate);
-    addStock("APPL", 12, currentDate.minusDays(1));
-    addStock("RBS.1", 6, currentDate.minusDays(45));
+    setCurrentUserToDefaultAdmin();
+    addStocksToCurrentUserPortfolio();
   }
 
-  private void addStock(String title, int quantity, LocalDate transactionDate) {
-    // Get current user
+  private void setCurrentUserToDefaultAdmin() {
     User user = null;
     try {
       user = serviceLocator.get(UserRepository.class).find(DEFAULT_ADMIN_EMAIL);
@@ -31,17 +31,26 @@ public class PortfolioReportITContext extends AbstractContext {
       e.printStackTrace();
     }
     serviceLocator.get(CurrentUserSession.class).setCurrentUser(user);
+  }
 
-    // Set clock to transaction date
+  private void addStocksToCurrentUserPortfolio() {
+    LocalDate currentDate = serviceLocator.get(Clock.class).getCurrentTime().toLocalDate();
+    addStock("RBS.l", 6, currentDate.minusDays(45));
+    addStock("AAPL", 12, currentDate.minusDays(15));
+    addStock("MSFT", 24, currentDate.minusDays(3));
+  }
+
+  private void addStock(String title, int quantity, LocalDate transactionDate) {
     Clock clock = serviceLocator.get(Clock.class);
     LocalDateTime oldDateTime = clock.getCurrentTime();
-    clock.setCurrentTime(transactionDate.atTime(0, 0, 0));
 
-    // Perform transaction
+    clock.setCurrentTime(transactionDate.atTime(0, 0, 0));
+    performTransaction(title, quantity);
+    clock.setCurrentTime(oldDateTime);
+  }
+
+  private void performTransaction(String title, int quantity) {
     serviceLocator.get(CartService.class).addStockToCart(title, quantity);
     serviceLocator.get(CheckoutService.class).checkoutCart();
-
-    // Revert clock to old time
-    clock.setCurrentTime(oldDateTime);
   }
 }
