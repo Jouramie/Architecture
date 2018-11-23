69cb53355eed198adf105489d25bd572e5027285
Émile Grégoire <eg@emilegregoire.ca>
2018-11-23 14:34:54 -0500

    Final fixes.

--- src/main/java/ca/ulaval/glo4003/ws/api/cart/mappers/HaltedMarketOnCheckoutExceptionMapper.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/cart/mappers/HaltedMarketOnCheckoutExceptionMapper.java
@@ -4,6 +4,7 @@ import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 
 import ca.ulaval.glo4003.service.cart.HaltedMarketOnCheckoutException;
 import ca.ulaval.glo4003.ws.api.ErrorMapper;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
@@ -11,6 +12,6 @@ import javax.ws.rs.ext.ExceptionMapper;
 public class HaltedMarketOnCheckoutExceptionMapper implements ExceptionMapper<HaltedMarketOnCheckoutException> {
   @Override
   public Response toResponse(HaltedMarketOnCheckoutException e) {
-    return Response.status(FORBIDDEN).entity(e.message).build();
+    return Response.status(FORBIDDEN).type(MediaType.TEXT_PLAIN_TYPE).entity(e.message).build();
   }
 }
