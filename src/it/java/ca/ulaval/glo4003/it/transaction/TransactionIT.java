2ba0efda6524d32cd6605aae13b2d3ac06976942
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-12-04 11:14:05 -0500

    route definition + IT

---
+++ src/it/java/ca/ulaval/glo4003/it/transaction/TransactionIT.java
@@ -0,0 +1,49 @@
+package ca.ulaval.glo4003.it.transaction;
+
+import static ca.ulaval.glo4003.context.AbstractContext.DEFAULT_INVESTOR_EMAIL;
+import static ca.ulaval.glo4003.it.util.UserAuthenticationHelper.givenInvestorAlreadyAuthenticated;
+import static ca.ulaval.glo4003.it.util.UserAuthenticationHelper.givenInvestorAlreadyRegistered;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static javax.ws.rs.core.Response.Status.OK;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import static org.assertj.core.util.IterableUtil.iterable;
+import static org.hamcrest.core.Is.is;
+
+import ca.ulaval.glo4003.it.ResetServerBetweenTest;
+import ca.ulaval.glo4003.ws.api.transaction.dto.TransactionModelDto;
+import io.restassured.http.Header;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class TransactionIT {
+
+  @ClassRule
+  public ResetServerBetweenTest resetServerBetweenTest = new ResetServerBetweenTest();
+
+  @Test
+  public void whenGettingUserTransactions_thenReturnListOfTransactions() {
+    givenInvestorAlreadyRegistered();
+    String token = givenInvestorAlreadyAuthenticated();
+    Header tokenHeader = new Header("token", token);
+    //@formatter:off
+    given()
+        .header(tokenHeader)
+    .when()
+        .get(String.format("/api/users/%s/transactions", DEFAULT_INVESTOR_EMAIL))
+    .then()
+        .statusCode(OK.getStatusCode())
+        .body("$", is(iterable(TransactionModelDto.class)));
+    //@formatter:on
+  }
+
+  @Test
+  public void givenUnauthenticatedUser_whenGettingTransactions_thenReturn401Unauthorized() {
+    //@formatter:off
+    when()
+        .get(String.format("/api/users/%s/transactions", DEFAULT_INVESTOR_EMAIL))
+    .then()
+        .statusCode(UNAUTHORIZED.getStatusCode());
+    //@formatter:on
+  }
+}
---
+++ src/main/java/ca/ulaval/glo4003/ws/api/transaction/TransactionResource.java
@@ -0,0 +1,44 @@
+package ca.ulaval.glo4003.ws.api.transaction;
+
+import ca.ulaval.glo4003.domain.user.UserRole;
+import ca.ulaval.glo4003.ws.api.transaction.dto.TransactionModelDto;
+import ca.ulaval.glo4003.ws.http.authentication.AuthenticationRequiredBinding;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Produces(MediaType.APPLICATION_JSON)
+public interface TransactionResource {
+
+  @Path("/users/{email}/transactions")
+  @GET
+  @AuthenticationRequiredBinding(acceptedRoles = {UserRole.INVESTOR, UserRole.ADMINISTRATOR})
+  @Operation(
+      summary = "Get transactions for a specific user.",
+      responses = {
+          @ApiResponse(
+              responseCode = "200",
+              content = @Content(
+                  array = @ArraySchema(
+                      schema = @Schema(
+                          implementation = TransactionModelDto.class
+                      )
+                  )
+              )
+          ),
+          @ApiResponse(
+              responseCode = "401",
+              description = "User is not logged in."
+          )
+      }
+  )
+  List<TransactionModelDto> getUserTransactions(@PathParam("email") String email);
+}
