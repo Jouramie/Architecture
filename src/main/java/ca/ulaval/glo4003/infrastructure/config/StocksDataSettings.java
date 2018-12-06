3d8c15ab66acc25a9e2242e95ffce64501611943
Émile Grégoire <eg@emilegregoire.ca>
2018-12-06 12:55:58 -0500

    CSV tests are now based on test data instead of real data.

---
+++ src/main/java/ca/ulaval/glo4003/infrastructure/config/StocksDataSettings.java
@@ -0,0 +1,10 @@
+package ca.ulaval.glo4003.infrastructure.config;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+public class StocksDataSettings {
+  public static final Path STOCKS_DATA_PATH = Paths.get("src", "main", "data");
+  public static final LocalDate LAST_STOCK_DATA_DATE = LocalDate.of(2018, 11, 16);
+}
