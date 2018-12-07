116143f52f21bf4af274a1375a0e5b8c84cfb737
Émile Grégoire <eg@emilegregoire.ca>
2018-12-07 09:58:57 -0500

    Updated to latest stocks data.

--- src/main/java/ca/ulaval/glo4003/infrastructure/config/StocksDataSettings.java
+++ src/main/java/ca/ulaval/glo4003/infrastructure/config/StocksDataSettings.java
@@ -6,5 +6,5 @@ import java.time.LocalDate;
 
 public class StocksDataSettings {
   public static final Path STOCKS_DATA_PATH = Paths.get("src", "main", "data");
-  public static final LocalDate LAST_STOCK_DATA_DATE = LocalDate.of(2018, 11, 16);
+  public static final LocalDate LAST_STOCK_DATA_DATE = LocalDate.of(2018, 11, 23);
 }
