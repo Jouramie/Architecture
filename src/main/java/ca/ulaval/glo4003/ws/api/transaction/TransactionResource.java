b9ea356bc371f4cdcc71314ae068bb5d06b18426
Émile Grégoire <eg@emilegregoire.ca>
2018-12-07 09:25:57 -0500

    Move transaction routes to appropriate resources.

--- src/main/java/ca/ulaval/glo4003/ws/api/transaction/TransactionResource.java
+++ src/main/java/ca/ulaval/glo4003/ws/api/transaction/TransactionResource.java
@@ -7,38 +7,18 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Produces(MediaType.APPLICATION_JSON)
 @Resource
-@Path("/")
+@Path("/transactions")
+@Produces(MediaType.APPLICATION_JSON)
 public class TransactionResource implements DocumentedTransactionResource {
   @GET
-  @Path("/transactions")
   @AuthenticationRequiredBinding(authorizedRoles = {UserRole.ADMINISTRATOR})
   @Override
   public List<TransactionModelDto> getTransactions(@QueryParam("since") String since) {
-    return null;
-  }
-
-  @GET
-  @Path("/users/{email}/transactions")
-  @AuthenticationRequiredBinding(authorizedRoles = UserRole.ADMINISTRATOR)
-  @Override
-  public List<TransactionModelDto> getUserTransactions(@PathParam("email") String email,
-                                                       @QueryParam("since") String since) {
-    return null;
-  }
-
-  @GET
-  @Path("/stocks/{title}/transactions")
-  @AuthenticationRequiredBinding(authorizedRoles = UserRole.ADMINISTRATOR)
-  @Override
-  public List<TransactionModelDto> getStockTransactions(@PathParam("title") String title,
-                                                        @QueryParam("since") String since) {
     return null;
   }
 }
