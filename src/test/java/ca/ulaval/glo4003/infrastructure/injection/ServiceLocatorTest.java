090aa8c6fb77be01c4f8e7a8d5af7676192396fb
Kento A. Lauzon <kento.lauzon@ligature.ca>
2018-12-14 11:36:05 -0500

    reset test

--- src/test/java/ca/ulaval/glo4003/infrastructure/injection/ServiceLocatorTest.java
+++ src/test/java/ca/ulaval/glo4003/infrastructure/injection/ServiceLocatorTest.java
@@ -1,20 +1,33 @@
 package ca.ulaval.glo4003.infrastructure.injection;
 
+import static org.assertj.core.api.Java6Assertions.assertThatThrownBy;
 import static org.junit.Assert.assertEquals;
 
+import ca.ulaval.glo4003.infrastructure.injection.exception.UnregisteredComponentException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ServiceLocatorTest {
 
+  public static final Object STORED_INSTANCE = new Object();
   private final ServiceLocator serviceLocator = new ServiceLocator();
+
+  @Before
+  public void setUp() {
+    serviceLocator.registerInstance(Object.class, STORED_INSTANCE);
+  }
 
   @Test
   public void whenGettingComponent_thenReturnStoredInstance() {
-    Object storedInstance = new Object();
-    serviceLocator.registerInstance(Object.class, storedInstance);
-
     Object gottenInstance = serviceLocator.get(Object.class);
 
-    assertEquals(storedInstance, gottenInstance);
+    assertEquals(STORED_INSTANCE, gottenInstance);
+  }
+
+  @Test
+  public void whenResetting_thenClearSavedInstances() {
+    serviceLocator.reset();
+
+    assertThatThrownBy(() -> serviceLocator.get(Object.class)).isInstanceOf(UnregisteredComponentException.class);
   }
 }
